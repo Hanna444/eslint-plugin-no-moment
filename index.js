656c4b35a3a1727dcc867a0cda248101da6eca8f
hanna444 <hannap19@ru.is>
2024-04-06 19:03:10 +0000

    take out stuff not in use

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
-const fs = require("fs");
-const path = require("path");
-
-// Read the contents of the current directory
-const ruleFiles = fs.readdirSync(__dirname)
-  // Filter out the current file (index.js), test files, and ensure it's not a directory
-  .filter(file => {
-    const fullPath = path.join(__dirname, file);
-    const isFile = fs.statSync(fullPath).isFile(); // Check if it's a file and not a directory
-    return isFile && file.endsWith('.js') && file !== "index.js" && !file.endsWith("test.js");
-  });
-
-module.exports = { rules };
+module.exports = {
+  rules: {
+    'no-moment': require('./no-moment')
+  }
+};
