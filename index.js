81e71b4f7efa0696d2cbe80d017504fc6fe5b009
hanna444 <hannap19@ru.is>
2024-04-06 18:34:12 +0000

    change index

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const fs = require("fs");
 const path = require("path");
 
-const ruleFiles = fs
-  .readdirSync(__dirname)
-  .filter((file) => file !== "index.js" && !file.endsWith("test.js"));
+// Read the contents of the current directory
+const ruleFiles = fs.readdirSync(__dirname)
+  // Filter out the current file (index.js), test files, and ensure it's not a directory
+  .filter(file => {
+    const fullPath = path.join(__dirname, file);
+    const isFile = fs.statSync(fullPath).isFile(); // Check if it's a file and not a directory
+    return isFile && file.endsWith('.js') && file !== "index.js" && !file.endsWith("test.js");
+  });
 
+// Create rules object
 const rules = Object.fromEntries(
-  ruleFiles.map((file) => [path.basename(file, ".js"), require("./" + file)]),
+  ruleFiles.map(file => {
+    const ruleName = path.basename(file, ".js");
+    return [ruleName, require("./" + file)];
+  }),
 );
 
 module.exports = { rules };
